72e8f28d3fad1297eeb5d002d77954154b8dd3fe
Muldoon, Matthew [USA] <Muldoon_Matthew@bah.com>
2020-08-17 08:33:32 -0400

    first commit

--- src/app-pages/Reports/index.js
+++ src/app-pages/Reports/index.js
@@ -1,8 +1,32 @@
 import React from "react";
+import TextSection from '../../app-components/TextSection';
+import SearchBox from '../../app-containers/SearchBox';
 
+/*
 export default () => (
   <main>
 
 <div>Reports</div>
   </main>
 );
+*/
+
+const ReportsPage = () => {
+
+	return (
+		<main>
+      <div className="header-section">
+      <TextSection
+				containerStyle={'text-left bg-gray-400 bg-opacity-50 px-8 lg:px-48 py-8'}
+				title={'Reports'}
+				titleStyle={'capitalize text-grey-900 text-2xl'}
+			/>
+      <div className="search-box-container py-4 px-6 mx-8 lg:mx-56 relative bg-white" style={{"top":"-30px"}}>
+        <SearchBox text={'Search Reports'}/>
+      </div>
+      </div>
+    </main>
+    );
+  };
+  
+  export default ReportsPage;
