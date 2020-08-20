f2690315194a1d957295060ca5988c196f5b2d07
604081 <danlin@C02Y90U6JGH7.local>
2020-08-20 00:08:15 -0400

    #2 add functions file

--- src/app-components/Cards/Cards.test.js
+++ src/app-components/Cards/Cards.test.js
@@ -7,7 +7,8 @@ import Cards from './index';
 const CardsArray = [
 	{ title: 'Locations', text: 'Card 1 Text', imgAlt: 'Card 1 Image', href: '/locations' },
 	{ title: 'Data Resources', text: 'Card 2 Text', imgAlt: 'Card 2 Image', href: '/data-resources' },
-	{ title: 'Reports', text: 'Card 3 Text', imgAlt: 'Card 3 Image', href: '/reports' },
+    { title: 'Reports', text: 'Card 3 Text', imgAlt: 'Card 3 Image', href: '/reports' },
+    { title: 'Reports', text: 'Card 3 Text', imgAlt: 'Card 3 Image', href: '/reports' },
 ];
 
 // Set up the component with props:
