031710df2e9077f9c6b272f9ba20b7f4788f5416
604081 <danlin@C02Y90U6JGH7.local>
2020-08-24 15:37:51 -0400

    #4 added more utility

--- src/app-pages/Home/index.js
+++ src/app-pages/Home/index.js
@@ -20,7 +20,8 @@ const HomePage = () => {
 	const options = { center: [-77.0364, 38.895], zoom: 4 };
 	return (
 		<main>
-			<div className="header-section p-5" style={headerContainerStyle}>
+			<div className="header-section" >
+				<div style={headerContainerStyle} className="p-5">
 				<TextSection
 					containerStyle={containerTextSection}
 					title={'find water resources data across the U.S.'}
@@ -28,27 +29,28 @@ const HomePage = () => {
 						'Access water resources data such as elevation, precipitation, storage, and flow status of more than 700 USACE reservoir and lock & dam projects.'
 					}
 				/>
+				</div>
+
 				<div
-					className="search-box-container py-4 px-4 mx-auto container"
-					style={{ 'top': '35px', 'zIndex': '1' }}
+					className="search-box-container py-4 px-4 mx-auto container position-relative"
+					
 				>
-					<SearchBox text={'Search by City, State, ZIP, or Project Names'} />
+					<div style={{ top: '100%', zIndex: '1'}}><SearchBox text={'Search by City, State, ZIP, or Project Names'} /></div>
+					
 				</div>
 			</div>
 
 			<Map mapKey={'home-page-map'} options={options} height={'600px'} />
 			<div className="container mx-auto px-5">
 					{cardObj && <Card cardObj={cardObj}/>}
-				<div className="container mx-auto mt-5">
+				<div className="container mx-auto my-5">
 					<TextSection
 						title={'The Mission of Access to Water'}
 						body={
 							'The United States Army Corps of Engineers (USACE) is responsible for operating and maintaining more than 700 lock and dam projects nationwide. The Access to Water Resources Data - Corps Water Management System (CWMS) Data Dissemination tool supports the USACE water control management mission by utilizing visualizations and reports to provide continuous assessment, awareness, and effective decision support of lock and dam projects, which in turn reduces risks to people, property, and the environment.'
 						}
 					/>
-					<div className="container my-5 mx-auto">
 					{circlePicObj && <CirclePics cardObj={circlePicObj}/>}
-					</div>
 				</div>
 			</div>
 		</main>
