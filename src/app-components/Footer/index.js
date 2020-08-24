2347b6a46b9d8163846a9b1ec61269210cae2a8d
604081 <danlin@C02Y90U6JGH7.local>
2020-08-24 11:42:53 -0400

    add bootstrap

--- src/app-components/Footer/index.js
+++ src/app-components/Footer/index.js
@@ -1,28 +1,81 @@
 import React from 'react';
 import TextSection from '../TextSection';
 
+const footerTextSectionContainer = {
+	height: 'auto',
+	borderTop: '8px solid #718096',
+	display: 'flex',
+	backgroundColor: '#4a5568',
+};
+
+const footerTextSectionOne = {
+width: '50%',
+marginBottom: '0.75rem',
+padding: '1.25rem 5rem 1.25rem 10rem'
+};
+
+const footerTextSectionTwo = {
+	width: '50%',
+marginBottom: '0.75rem',
+padding: '1.25rem 10rem 1.25rem 5rem'
+}
+
+const footerSectionTwo = {
+	height: '6rem',
+	backgroundColor: '#2d3748'
+
+}
+
+const textSectionBodyStyle = {
+	textAlign:'left', color: '#cbd5e0', fontSize: '.875rem', marginTop: '0.5rem'
+}
+
+const textSectionTitleStyle = {
+	textTransform: 'capitalize',
+	textAlign:'left',
+	fontSize: '1rem',
+	color: '#a0aec0'
+}
+
+const linkColStyle = {
+	height: '100%',
+	width: '33%',
+	color: 'white'
+}
 const Footer = (props) => {
 	return (
 		<div data-test="component-footer-container" className="footer-container">
-			<div className="bg-gray-700 h-auto border-t-8 border-gray-600 md:flex">
-				<div className="misson-statement py-5 pl-40 pr-20 mb-3 md:w-1/2">
-					<TextSection title={'our mission'} titleStyle={'text-gray-500 text-md text-left capitalize'} bodyStyle={'text-left text-gray-400 text-sm mt-2'} body={'The mission of the U.S. Army Corps of Engineers is to deliver vital public and military engineering services; partnering in peace and war to strengthen our nation\'s security. energize the economy and reduce risks from disasters.'}/>
-					<TextSection title={'about the access to water website'} titleStyle={'text-gray-500 text-md text-left capitalize'} bodyStyle={'text-left text-gray-400 text-sm mt-2'} body={'The CWMS Data Dissemination website is a public accessible map based repository of water resources data that includes elevation, precipation, storage, and flow status of USACE reservoir and lock & dam projects.'}/>
+			<div style={footerTextSectionContainer}>
+				<div style={footerTextSectionOne} className="misson-statement">
+					<TextSection
+						title={'our mission'}
+						titleStyle={textSectionTitleStyle}
+						bodyStyle={textSectionBodyStyle }
+						body={
+							"The mission of the U.S. Army Corps of Engineers is to deliver vital public and military engineering services; partnering in peace and war to strengthen our nation's security. energize the economy and reduce risks from disasters."
+						}
+					/>
+					<TextSection
+						title={'about the access to water website'}
+						titleStyle={textSectionTitleStyle}
+						bodyStyle={textSectionBodyStyle }
+						body={
+							'The CWMS Data Dissemination website is a public accessible map based repository of water resources data that includes elevation, precipation, storage, and flow status of USACE reservoir and lock & dam projects.'
+						}
+					/>
 				</div>
-				<div className="links p-5 py-5 pr-40 pl-20 pb-3 md:w-1/2">
-        <div className="icon-container"></div>
-        <div className="flex flex-row">
-          <div className="link-col w-1/3 h-full text-white">contact us</div>
-          <div className="link-col w-1/3 h-full text-white">contact us</div>
-          <div className="link-col w-1/3 h-full text-white">contact us</div>
-          </div>
-        </div>
+				<div style={footerTextSectionTwo}>
+					<div className="icon-container"></div>
+					<div className="flex flex-row">
+						<div style={linkColStyle} className="link-col">contact us</div>
+						<div style={linkColStyle} className="link-col">contact us</div>
+						<div style={linkColStyle} className="link-col">contact us</div>
+					</div>
+				</div>
 			</div>
-			<div className="bg-gray-800 h-24"></div>
+			<div style={footerSectionTwo}></div>
 		</div>
 	);
 };
-
-
 
 export default Footer;
--- src/app-pages/Home/index.js
+++ src/app-pages/Home/index.js
@@ -6,22 +6,27 @@ import Map from '../../app-containers/Map';
 import SearchBox from '../../app-containers/SearchBox';
 import { circlePicObj, cardObj } from './data.js';
 
+const containerTextSection = {
+	textAlign: 'center',
+	margin:'1rem 0',
+	padding: 'auto 12rem',
+	width: '100%'
+}
 const HomePage = () => {
 	const options = { center: [-77.0364, 38.895], zoom: 4 };
 	return (
 		<main>
 			<div className="header-section bg-gray-400 bg-opacity-50xs">
 				<TextSection
-					containerStyle={'text-center px-8 lg:px-48 pt-8 mx-auto container'}
+					containerStyle={containerTextSection}
 					title={'find water resources data across the U.S.'}
-					titleStyle={'capitalize text-grey-900 text-2xl'}
 					body={
 						'Access water resources data such as elevation, precipitation, storage, and flow status of more than 700 USACE reservoir and lock & dam projects.'
 					}
 				/>
 				<div
-					className="search-box-container py-4 px-4 relative bg-white mx-auto container"
-					style={{ top: '35px', 'zIndex': '1' }}
+					className="search-box-container py-4 px-4 mx-auto container"
+					style={{ 'top': '35px', 'zIndex': '1' }}
 				>
 					<SearchBox text={'Search by City, State, ZIP, or Project Names'} />
 				</div>
@@ -29,16 +34,13 @@ const HomePage = () => {
 
 			<Map mapKey={'home-page-map'} options={options} height={'600px'} />
 			<div className="container mx-auto px-20">
-				<div className="container mt-24 mx-auto lg:flex space-x-6">
 					{cardObj && <Card cardObj={cardObj}/>}
-				</div>
 				<div className="container mx-auto mt-24">
 					<TextSection
 						title={'The Mission of Access to Water'}
 						body={
 							'The United States Army Corps of Engineers (USACE) is responsible for operating and maintaining more than 700 lock and dam projects nationwide. The Access to Water Resources Data - Corps Water Management System (CWMS) Data Dissemination tool supports the USACE water control management mission by utilizing visualizations and reports to provide continuous assessment, awareness, and effective decision support of lock and dam projects, which in turn reduces risks to people, property, and the environment.'
 						}
-						bodyStyle={'body-text text-left'}
 					/>
 					<div className="container my-24 mx-auto lg:flex lg:space-x-32">
 					{circlePicObj && <CirclePics cardObj={circlePicObj}/>}
