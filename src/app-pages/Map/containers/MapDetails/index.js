40e2a3e35bd31c5ea2526424067193885efe2f18
604081 <danlin@C02Y90U6JGH7.local>
2020-09-04 03:02:23 -0400

    #18 rough adjustment of animation

--- src/app-pages/Map/containers/MapDetails/index.js
+++ src/app-pages/Map/containers/MapDetails/index.js
@@ -8,7 +8,14 @@ const MapDetails = (props) => {
 
 	return (
 		<div className={`map-details-wrapper ${isOpen ? 'is-expanded' : ''}`} onClick={toggleDrawer}>
-			<div className={`drawer-icon-container ${isOpen ? 'is-expanded-icon-container' : ''}`}>
+
+			<div className="drawer-content-container">
+      <div className="drawer-content">
+				{/* {children} */}
+				Map Details page
+      </div>
+<div className="outer-container">
+        <div className="drawer-icon-container">
 				<div className="icons mdi-chart-arc"></div>
 				<div className="icons mdi-chart-arc"></div>
 				<div className="icons mdi-chart-arc"></div>
@@ -16,9 +23,7 @@ const MapDetails = (props) => {
 				<div className="icons mdi-chart-arc"></div>
 				<div className="icons mdi-chart-arc"></div>
 			</div>
-			<div className="drawer-content">
-				{/* {children} */}
-				Map Details page
+      </div>
 			</div>
 		</div>
 	);
