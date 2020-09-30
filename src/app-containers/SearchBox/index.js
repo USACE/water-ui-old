7e27bfc27509fef91821a1b14302fde2578dedb7
Muldoon, Matthew [USA] <Muldoon_Matthew@bah.com>
2020-09-30 09:45:49 -0400

    added onChange,onEnterKey,value and handling

--- src/app-containers/SearchBox/index.js
+++ src/app-containers/SearchBox/index.js
@@ -3,24 +3,27 @@ import PropTypes from 'prop-types';
 import classnames from "classnames";
 import './searchbox.css';
 
-const SearchBox = ({text='search',theme='light'}) => {
+const SearchBox = ({value,onChange,onEnterKey,text='search'}) => {
+
 
 	const handleKeyDown = (event) => {
-		var searchString = event.target.value;
-		if (event.key === 'Enter') {
-		  //console.log(searchString);
-		  //do any action
+    var searchString = event.target.value;
+    onChange(); //call onChange method - saves new input?
+		if (event.key === 'Enter') { //if key pressed Enter, call onEnterKey
+		  console.log(searchString);
+      //does any submit action
+      onEnterKey();
 		}
-	  }
+  }
 
-	const searchClass = classnames({
-		'bg-white focus:outline-none focus:shadow-outline border border-gray-500 py-2 px-4 block w-full appearance-none leading-normal': theme === 'light',
-		'bg-gray-700 focus:outline-none focus:shadow-outline py-2 px-4 block w-full appearance-none leading-normal': theme === 'dark'
-	  });
+
+	const searchClass = classnames( //removed :theme
+		'bg-white focus:outline-none focus:shadow-outline border border-gray-500 py-2 px-4 block w-full appearance-none leading-normal'
+	);
 
 	return (
 		<div className="search-box-container" data-test='search-box-container'>
-            <input style={{width:'100%'}} className={searchClass} type="search" placeholder={text} onKeyDown={handleKeyDown}/>
+      <input style={{width:'100%'}} className={searchClass} type="search" placeholder={text} onKeyDown={handleKeyDown} defaultValue={value}/>
 		</div>
 	);
 
@@ -30,7 +33,7 @@ SearchBox.defaultProps = {
   text: "Enter search text",
   value: "",
   onChange: ( event ) => null,
-  onKeyDown: ( event ) => null,
+  onEnterKey: ( event ) => null,
 };
 
 SearchBox.propTypes = {
