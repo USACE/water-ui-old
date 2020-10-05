518cf73b28c20109fb32d84ff526e387a1816a8c
brian.kotek <kotek_brian@bah.com>
2020-10-05 15:19:50 -0400

    #27 initial location detail sidebar header

---
+++ src/app-components/LocationDetail/Header/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import "../location-detail.css";
+
+const LocationDetailHeader = ( { onExpand } ) => {
+
+  // Temporary until we add the rest bundle to load location detail
+  const locationDetail = {
+    office_id: "MVP",
+    office_name: "St. Paul District",
+    public_name: "Mississippi R Lock and Dam 03",
+    long_name: "Mississippi River Lock and Dam 03",
+    location_code: "4572016",
+    location_id: "LockDam_03",
+    location_kind_id: "PROJECT",
+    latitude: 44.61,
+    longitude: -92.61,
+    elevation: 600,
+    unit_id: "ft",
+    time_zone_name: "US/Central",
+    county_name: "Goodhue",
+    state: "MN",
+    nearest_city: "Red Wing"
+  };
+
+  const weatherUrl = `https://forecast.weather.gov/MapClick.php?CityName=${ locationDetail.nearest_city }&state=${ locationDetail.state }`
+
+  const onExpandHandler = ( event ) => {
+    event.preventDefault();
+    event.stopPropagation();
+    onExpand();
+  }
+
+  return (
+    <div className="location-detail-header">
+      <div className="mdi mdi-arrow-expand location-expand" title="Expand Detail View" onClick={ onExpandHandler }></div>
+      <h4>{ locationDetail.public_name }</h4>
+      <div className="location-detail-subheading">
+        { locationDetail.office_name } | <a href={ weatherUrl } target="_blank">Local Forecast</a>
+      </div>
+    </div>
+  );
+};
+
+export default LocationDetailHeader;
+
+LocationDetailHeader.defaultProps = {
+  onExpand: () => null
+}
+
+LocationDetailHeader.propTypes = {
+  onExpand: PropTypes.func
+}
--- src/app-pages/Map/containers/MapDetails/index.js
+++ src/app-pages/Map/containers/MapDetails/index.js
@@ -1,32 +1,33 @@
 import React, { useState } from 'react';
 import './mapDetails.css';
+import LocationDetailHeader from "../../../../app-components/LocationDetail/Header";
 // import PropTypes from 'prop-types';
 
-const MapDetails = (props) => {
-	const [isOpen, setIsOpen] = useState(false);
-	const toggleDrawer = () => setIsOpen(!isOpen);
+const MapDetails = ( props ) => {
+  const [ isOpen, setIsOpen ] = useState( false );
+  const toggleDrawer = () => setIsOpen( !isOpen );
 
-	return (
-		<div className={`map-details-wrapper ${isOpen ? 'is-expanded' : ''}`} onClick={toggleDrawer}>
+  return (
+    <div className={ `map-details-wrapper ${ isOpen ? 'is-expanded' : '' }` } onClick={ toggleDrawer }>
+      <div className="drawer-content-container">
 
-			<div className="drawer-content-container">
-      <div className="drawer-content">
-				{/* {children} */}
-				Map Details page
+        <div className="drawer-content">
+          <LocationDetailHeader></LocationDetailHeader>
+        </div>
+
+        <div className="outer-container">
+          <div className="drawer-icon-container">
+            <div className="icons mdi mdi-water-pump" title="Dam Profile"></div>
+            <div className="icons mdi mdi-chart-line" title="Time Series"></div>
+            <div className="icons mdi mdi-map-marker" title="Location"></div>
+            <div className="icons mdi mdi-layers" title="Sedimentation"></div>
+            <div className="icons mdi mdi-blur-linear" title="Grab Samples"></div>
+            <div className="icons mdi mdi-crop-square" title="Box Plots"></div>
+          </div>
+        </div>
       </div>
-<div className="outer-container">
-        <div className="drawer-icon-container">
-				<div className="icons mdi mdi-water-pump" title="Dam Profile"></div>
-				<div className="icons mdi mdi-chart-line" title="Time Series"></div>
-				<div className="icons mdi mdi-map-marker" title="Location"></div>
-				<div className="icons mdi mdi-layers" title="Sedimentation"></div>
-				<div className="icons mdi mdi-blur-linear" title="Grab Samples"></div>
-				<div className="icons mdi mdi-crop-square" title="Box Plots"></div>
-			</div>
-      </div>
-			</div>
-		</div>
-	);
+    </div>
+  );
 };
 
 // MapDetails.propTypes = {
