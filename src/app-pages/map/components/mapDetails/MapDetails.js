0e0f87f9fcbd61b30f1106b1527108c91a540c73
mohitb3 <bisht_mohit@bah.com>
2020-10-07 03:13:18 -0400

    work around to rename directory to lower case

--- src/app-pages/map/components/mapDetails/MapDetails.js
+++ src/app-pages/map/components/mapDetails/MapDetails.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./mapDetails.css";
-import LocationDetailHeader from "../../../../app-components/LocationDetail/Header";
-import Accordion from "../../../../app-components/Accordion/Accordion";
+import LocationDetailHeader from "../../../../app-components/LocationDetail2/Header";
+import Accordion from "../../../../app-components/Accordion2/Accordion";
 // import PropTypes from 'prop-types';
 
 const MapDetails = (props) => {
