a936117ddf4b30e98ca05ecc7ea6765fe089a6d7
604081 <danlin@C02Y90U6JGH7.local>
2020-10-11 20:44:35 -0400

    hooked up location details to table

--- src/app-common/table/Table.js
+++ src/app-common/table/Table.js
@@ -1,69 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 // CSS
-import "./table.css";
+import "./table.scss";
 
-const json = {
-  office_id: "MVP",
-  office_name: "St. Paul District",
-  public_name: "Mississippi R Lock and Dam 03",
-  long_name: "Mississippi River Lock and Dam 03",
-  location_code: "4572016",
-  location_id: "LockDam_03",
-  location_kind_id: "PROJECT",
-  latitude: 44.61,
-  longitude: -92.61,
-  elevation: 600,
-  unit_id: "ft",
-  time_zone_name: "US/Central",
-  county_name: "Goodhue",
-  state: "MN",
-  nearest_city: "Red Wing",
-  flood_storage_pct: 20,
-  current_elevation: 374.2,
-  daily_elevation_change: -1.37,
-  dam_profile: {
-    history: [
-      {
-        date: "2020-10-07T16:17:59.630Z",
-        in_flow: 123,
-        top_of_dam: 452.2,
-        top_of_flood: 435,
-        bottom_of_flood: 374.26,
-        stream_bed: 369.2,
-        out_flow: 123,
-      },
-    ],
-  },
-};
 const Table = (props) => {
-  // const { data } = props;
-  const tableHeader = ["name", "value"];
+  const { headerRowArr, rowsArr} = props;
 
   return (
     <div className="table-container" data-test="table-container">
       <table>
-        <tr>
-          {tableHeader.map((item) => {
-            return <th>{item}</th>;
-          })}
-        </tr>
-        {json.map((item) => {
-          return (
-            <tr>
-              <td>January</td>
-              <td>$100</td>
+        <thead>
+          <tr className="table-header-row">
+            {headerRowArr &&
+              headerRowArr.map((item, i) => {
+                return <th key={`header-${i}`}>{item}</th>;
+              })}
+          </tr>
+        </thead>
+        <tbody>
+          {rowsArr && rowsArr.map((row, i) => (
+            <tr key={`row-${i}`} className="table-body-row">
+              {row.map((item, i) => {
+                return <td key={`td-${i}`}>{item}</td>;
+              })}
             </tr>
-          );
-        })}
+          ))}
+        </tbody>
       </table>
     </div>
   );
 };
 
 Table.propTypes = {
-  // data: PropTypes.array,
+  rowsArr:PropTypes.array,
+  headerRowArr:PropTypes.array
 };
 
 export default Table;
--- src/app-pages/map/components/LocationInfo.js
+++ src/app-pages/map/components/LocationInfo.js
@@ -1,19 +1,34 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useEffect,useState } from "react";
+import PropTypes from 'prop-types';
 import Table from "../../../app-common/table/Table";
+import { connect } from "redux-bundler-react";
 
 
-const LocationInfo = (props) => (
-  <div className="LocationInfoWrapper">
-    <h2>Metadata</h2>
-    <Table/>
-  </div>
+const metaDataHeaderArr = ["name", "value"];
+let metaDataRowsArr;
+
+const LocationInfo = ({selectedLocationDetail}) => {
+
+const [metaDataState, setmetaDataState] = useState();
+  useEffect (() => {
+  const metaDataJsonObj = selectedLocationDetail;
+  delete metaDataJsonObj.dam_profile;
+  metaDataRowsArr = Object.entries(metaDataJsonObj);
+  setmetaDataState(metaDataRowsArr)
+  },[selectedLocationDetail]);
+
+  return (
+    <div className="LocationInfoWrapper">
+      <h5>Metadata</h5>
+    {metaDataState ? <Table rowsArr={metaDataState} headerRowArr={metaDataHeaderArr} /> :null}
+    </div>
+  );
+};
+LocationInfo.propTypes = {
+  selectedlocationDetail: PropTypes.object,
+};
+
+export default connect(
+  "selectSelectedLocationDetail",
+  LocationInfo
 );
-
-// LocationInfo.propTypes = {
-//   // bla: PropTypes.string,
-// };
-
-
-
-export default LocationInfo;
