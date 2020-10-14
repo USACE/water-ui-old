1d85331bc25a6dc1f0c47afc3bd085dca40b8375
604081 <danlin@C02Y90U6JGH7.local>
2020-10-14 01:47:09 -0400

    toggle an array of accordions

--- src/app-common/TabLinks.js
+++ src/app-common/TabLinks.js
@@ -10,7 +10,7 @@ const TabLinks = ({ TabInfo }) => {
       <div className="container-fluid">
         <div className="row">
           <ul className="list-group list-group-horizontal flex-row" id="list-tab" role="tablist">
-              {TabInfo.tabLinks.map(({ name, url }) => (
+              {TabInfo && TabInfo.tabLinks && TabInfo.tabLinks.map(({ name, url }) => (
                 <div key={ name }>
                   <a className={`list-group-item list-group-item-action ${name === TabInfo.activeTab ? ' active' : ''}`} data-toggle="list" role="tab" href={url} >{name}
                     {TabInfo.tabLinks.length === 3 ? <span className="badge badge-pill badge-dark" style={ inlinePill }>0</span> : ''}
