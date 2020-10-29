38218fb9d7c61c892d61bd2e06714d8171735de4
604081 <dlin2102@gmail.com>
2020-10-29 12:27:56 -0400

    change to doUpdateUrl

--- src/app-pages/map/components/map-details/MapDetails.js
+++ src/app-pages/map/components/map-details/MapDetails.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "redux-bundler-react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./mapDetails.scss";
 import MapDetailsContent from "../../../../app-common/map-details-container/MapDetailsContent";
 import { RoutePaths } from "../../../../app-bundles/routes-bundle";
 
-const MapDetails = ({
-  doSetSelectedLocationCode,
-  selectedLocationCode,
-  selectedLocationDetail,
-}) => {
+const MapDetails = ({ doSetSelectedLocationCode, selectedLocationCode, selectedLocationDetail, doUpdateUrl }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isFullScreen, setIsFullScreen] = useState(false);
   const toggleDrawer = () => {
@@ -23,49 +19,46 @@ const MapDetails = ({
   });
 
   /** @type {React.CSSProperties} */
-  const mapDetailsStyle = Object.keys(selectedLocationDetail) && Object.keys(selectedLocationDetail).length > 0
-    ? { padding: 0, flexGrow: 1 }
-    : { visibility: "hidden" };
+  const mapDetailsStyle =
+    Object.keys(selectedLocationDetail) && Object.keys(selectedLocationDetail).length > 0
+      ? { padding: 0, flexGrow: 1 }
+      : { visibility: "hidden" };
 
-  const handleFullScreen = (selectedLocationCode) => {
+  const handleFullScreen = (selectedLocationCode, doUpdateUrl) => {
     setIsFullScreen(true);
-    const newLocation = `${ RoutePaths.Locations.replace( ":locationId", selectedLocationCode ) }`;
+    const newLocation = `${RoutePaths.Locations.replace(":locationId", selectedLocationCode)}`;
+
     //set timeout to allow css transition to happen
-    setTimeout(function(){   
-        document.body.classList.toggle('fade'); 
-        window.location.href = newLocation;     
+    // added simple fade, will replace later.
+    setTimeout(function () {
+      document.body.classList.toggle("fade");
+      doUpdateUrl(newLocation);
+      setTimeout(function () {
+        document.body.classList.toggle("fade");
+      }, 700);
     }, 500);
   };
-
   return (
     <div className="map-details" style={mapDetailsStyle}>
       <div className="map-details-wrapper">
         <div className={`${isOpen ? "is-expanded" : ""}`} onClick={toggleDrawer}>
           <div className={`${isFullScreen ? "full-screen " : "drawer-content-container"}`}>
             <div className={`${isOpen ? "drawer-content" : "display-none"}`}>
-              <MapDetailsContent handleFullScreen={()=>handleFullScreen(selectedLocationCode)} locationDetail={selectedLocationDetail} locationCode={selectedLocationCode}/>
+              <MapDetailsContent
+                handleFullScreen={() => handleFullScreen(selectedLocationCode, doUpdateUrl)}
+                locationDetail={selectedLocationDetail}
+                locationCode={selectedLocationCode}
+              />
             </div>
 
             <div className="outer-container">
               <div className="drawer-icon-container">
-                <div
-                  className="icons mdi mdi-water-pump"
-                  title="Dam Profile"
-                ></div>
-                <div
-                  className="icons mdi mdi-chart-line"
-                  title="Time Series"
-                ></div>
+                <div className="icons mdi mdi-water-pump" title="Dam Profile"></div>
+                <div className="icons mdi mdi-chart-line" title="Time Series"></div>
                 <div className="icons mdi mdi-map-marker" title="Location"></div>
                 <div className="icons mdi mdi-layers" title="Sedimentation"></div>
-                <div
-                  className="icons mdi mdi-blur-linear"
-                  title="Grab Samples"
-                ></div>
-                <div
-                  className="icons mdi mdi-crop-square"
-                  title="Box Plots"
-                ></div>
+                <div className="icons mdi mdi-blur-linear" title="Grab Samples"></div>
+                <div className="icons mdi mdi-crop-square" title="Box Plots"></div>
               </div>
             </div>
           </div>
@@ -85,5 +78,6 @@ export default connect(
   "doSetSelectedLocationCode",
   "selectSelectedLocationCode",
   "selectSelectedLocationDetail",
+  "doUpdateUrl",
   MapDetails
 );
