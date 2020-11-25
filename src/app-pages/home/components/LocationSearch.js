55f377ead74bf39d1031c4874278e8b87bde3984
mohitb3 <bisht_mohit@bah.com>
2020-11-25 08:34:21 -0500

    change LocationSearch to go to map route when user selects a location

--- src/app-pages/home/components/LocationSearch.js
+++ src/app-pages/home/components/LocationSearch.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "redux-bundler-react";
 import Autocomplete from "../../../app-common/inputs/autocomplete/Autocomplete";
-import { RoutePaths } from "../../../app-bundles/route-paths";
+import { defaultMapParams, getMapUrl } from "../../map/utils";
 
 const LocationSearch = ({
   locationSearchText,
@@ -19,24 +19,34 @@ const LocationSearch = ({
 
   let items = [];
   if( locationSearchText ) {
-    items = locationSearchData.map( ( { description, location_id, nearest_city, county_name } ) => {
+    items = locationSearchData.map( ( { description, nearest_city, county_name }, index ) => {
       let additionalText = [];
-      if( nearest_city ) additionalText.push( `near ${ nearest_city }` )
-      if( county_name ) additionalText.push( `${ county_name } county` )
+      if( nearest_city ) additionalText.push( `near ${ nearest_city }` );
+      if( county_name ) additionalText.push( `${ county_name } county` );
 
-      let dispVal = <>
-        <span style={{ marginRight: "5px" }}>{description}</span>
-        <span style={{ fontSize: ".8rem" }}>{additionalText.length ? `(${ additionalText.join( ', ' ) })` : "" }</span>
-      </>
+      const dispVal = (
+        <>
+          <span style={{ marginRight: "5px" }}>{description}</span>
+          <span style={{ fontSize: ".8rem" }}>{additionalText.length ? `(${ additionalText.join( ', ' ) })` : "" }</span>
+        </>
+      );
 
-      return ( { value: location_id, display: dispVal } )
+      return ( { value: index, display: dispVal } );
     } )
   }
 
   const itemOnClick = (e) => {
-    const locationCode = e.currentTarget.value || e.target.value;
-    doSetLocationSearchText("");
-    doUpdateUrl(RoutePaths.Locations.replace(":locationId", locationCode));
+    const index = e.currentTarget.value || e.target.value;
+    const location = locationSearchData[index];
+    const mapParams = {
+      ...defaultMapParams,
+      locationId: location.location_id,
+      lat: location.latitude,
+      lon: location.longitude,
+      zoom: location.zoom_depth,
+    };
+    const mapUrl = getMapUrl(mapParams);
+    doUpdateUrl(mapUrl);
   }
 
   return (
