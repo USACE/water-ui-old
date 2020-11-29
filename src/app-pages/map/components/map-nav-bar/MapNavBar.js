886d82f7fa143c9cfb982b9fb394778463a05044
mohitb3 <bisht_mohit@bah.com>
2020-11-28 21:49:43 -0500

    add LocationSearch to the MapNavbar

--- src/app-pages/map/components/map-nav-bar/MapNavBar.js
+++ src/app-pages/map/components/map-nav-bar/MapNavBar.js
@@ -1,54 +1,62 @@
 import React from "react";
-import SearchBox from "../../../../app-common/inputs/SearchBox";
+import PropTypes from "prop-types";
+import { connect } from "redux-bundler-react";
+import debounce from "lodash/debounce";
+import LocationSearch from "../../../home/components/LocationSearch";
 import Dropdown from "../../../../app-common/inputs/Dropdown";
 import LocationTree from "./LocationTree";
 import LocationTypeFilter from "./LocationTypeFilter";
 import "./mapNavBar.scss";
 
-const MapNavBar = ({ queryObject }) => (
-  <div className="map-navbar">
-    <div className="h-100 px-5 py-4 bg-secondary">
-      <div className="row map-nav-row">
-        <div className="col-md-4 mb-3">
-          <SearchBox />
-        </div>
-        <div className="col-md-1">
-          <Dropdown
-            label="Districts Dropdown"
-            id="districts-dropdown"
-            placeholder="Select District..."
-            options={[
-              { id: "1", value: "1" },
-              { id: "2", value: "2" },
-              { id: "3", value: "3" },
-            ]}
-          />
-        </div>
-        <div className="col-md-1">
-          <Dropdown
-            id="basins-dropdown"
-            label="Basin Dropdown"
-            placeholder="Select Basin..."
-            options={[
-              { id: "1", value: "1" },
-              { id: "2", value: "2" },
-              { id: "3", value: "3" },
-            ]}
-          />
-        </div>
-        <div className="col-md-2">
-          <LocationTypeFilter
-            queryObject={queryObject}
-          />
-        </div>
-        <div className="col-md-4">
-          <LocationTree
-            queryObject={queryObject}
-          />
+const MapNavbar = ({ doSetLocationSearchCriteriaUpdated }) => {
+  const debounceFetch = debounce(doSetLocationSearchCriteriaUpdated, 500);
+  return (
+    <div className="map-navbar">
+      <div className="h-100 px-5 py-4 bg-secondary">
+        <div className="row map-nav-row">
+          <div className="col-md-4">
+            <LocationSearch
+              debounceFetch={debounceFetch}
+            />
+          </div>
+          <div className="col-md-1">
+            <Dropdown
+              label="Districts Dropdown"
+              id="districts-dropdown"
+              placeholder="Select District..."
+              options={[
+                { id: "1", value: "1" },
+                { id: "2", value: "2" },
+                { id: "3", value: "3" },
+              ]}
+            />
+          </div>
+          <div className="col-md-1">
+            <Dropdown
+              id="basins-dropdown"
+              label="Basin Dropdown"
+              placeholder="Select Basin..."
+              options={[
+                { id: "1", value: "1" },
+                { id: "2", value: "2" },
+                { id: "3", value: "3" },
+              ]}
+            />
+          </div>
+          <div className="col-md-2">
+            <LocationTypeFilter />
+          </div>
+          <div className="col-md-4">
+            <LocationTree />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default MapNavBar;
+MapNavbar.propTypes = {
+  doSetLocationSearchCriteriaUpdated: PropTypes.func.isRequired,
+};
+
+export default connect("doSetLocationSearchCriteriaUpdated", MapNavbar);
