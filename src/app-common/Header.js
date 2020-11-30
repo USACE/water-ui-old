20c55bf3370e5b7b183ec3d7fbbb82824fabb7c7
mohitb3 <bisht_mohit@bah.com>
2020-11-30 15:16:51 -0500

    remove the search bar from the navbar for now since it doesn't do anything

--- src/app-common/Header.js
+++ src/app-common/Header.js
@@ -55,14 +55,14 @@ const Header = ({ pathname }) => {
 							</a>
 						</li>
 					</ul>
-					{ pathname !== RoutePaths.Home && pathname !== "/" && (
+					{/* { pathname !== RoutePaths.Home && pathname !== "/" && (
 						<form className="form-inline my-2 my-lg-0">
 							<input className="form-control mr-sm-2" type="text" placeholder="Search" />
 							<button className="btn btn-secondary my-2 my-sm-0" type="submit">
 								Search
 							</button>
 						</form>
-					)}
+					)} */}
 				</div>
 			</nav>
 		</header>
