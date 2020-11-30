414c7d41ebb9f43f887ef225aef772b81b451192
mohitb3 <bisht_mohit@bah.com>
2020-11-30 15:09:48 -0500

    add the card back for now

--- src/app-pages/home/data.js
+++ src/app-pages/home/data.js
@@ -2,10 +2,12 @@ import waterImage from '../../img/big-sky-river.jpg';
 import { RoutePaths } from "../../app-bundles/route-paths";
 
 export const cardObj = [
+    { title: 'Locations', text: 'Card 1 Text', img: waterImage, imgAlt: 'Card 1 Image', href: RoutePaths.Map },
     { title: 'Data Resources', text: 'Card 2 Text', img: waterImage, imgAlt: 'Card 2 Image', href: RoutePaths.DataResources },
     { title: 'Reports', text: 'Card 3 Text', img: waterImage, imgAlt: 'Card 3 Image', href: RoutePaths.ReportsHome },
 ];
 export const circlePicObj = [
+    { title: 'Water Quality Assessment', img: waterImage, imgAlt: 'Card 1 Image', href: '/#link1' },
     { title: 'Water Quality Awareness', img: waterImage, imgAlt: 'Card 2 Image', href: '/#link2' },
     { title: 'Water Quality Support', img: waterImage, imgAlt: 'Card 3 Image', href: '/#link3' },
 ];
