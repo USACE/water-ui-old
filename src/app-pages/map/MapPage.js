f372d5823387f77367428a31122e5e006eaf01c4
mohitb3 <bisht_mohit@bah.com>
2020-12-16 12:29:26 -0500

    move the fetch calls for the location detail and location level into the MapDetailsContainer

--- src/app-pages/map/MapPage.js
+++ src/app-pages/map/MapPage.js
@@ -1,42 +1,16 @@
-import React, { useEffect } from "react";
-import PropTypes from "prop-types";
-import { connect } from "redux-bundler-react";
+import React from "react";
 import MapNavbar from "./components/map-navbar/MapNavbar";
 import MapDetailsContainer from "./components/map-details/MapDetailsContainer";
 import Map from "./components/map/Map";
 
-const MapPage = ({
-  queryObject,
-  doLocationDetailFetch,
-  doLocationLevelFetch,
-}) => {
-  const locationId = queryObject.locationId || "";
-  useEffect(() => {
-    doLocationDetailFetch();
-    doLocationLevelFetch();
-  }, [locationId, doLocationDetailFetch, doLocationLevelFetch])
-
-  return (
-    <>
-      <MapNavbar />
-      <div>
-        <MapDetailsContainer />
-        <Map />
-      </div>
-    </>
-  );
-};
-
-MapPage.propTypes = {
-  queryObject: PropTypes.shape({
-    locationId: PropTypes.string,
-  }).isRequired,
-  doLocationDetailFetch: PropTypes.func.isRequired,
-};
-
-export default connect(
-  "selectQueryObject",
-  "doLocationDetailFetch",
-  "doLocationLevelFetch",
-  MapPage
+const MapPage = () => (
+  <>
+    <MapNavbar />
+    <div>
+      <MapDetailsContainer />
+      <Map />
+    </div>
+  </>
 );
+
+export default MapPage;
